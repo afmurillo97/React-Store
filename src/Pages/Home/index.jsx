bcca949df8aaae6b585ed2e23739080f814d6734
afmurillo97 <afmurillo97@gmail.com>
2024-05-27 20:10:01 -0500

    use fakeApi to show products in store

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,32 @@
+import { useState, useEffect } from 'react';
+import Layout from '../../Components/Layout';
+import Card from '../../Components/Card';
+import ProductDetail from '../../Components/ProductDetail';
+
 function Home() {
 
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(data => setItems(data))
+
+  }, []);
+
   return (
-    <div className='bg-red-100'>
+    <Layout>
       Home
-    </div>
+      <div className='grid gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-screen-lg mx-auto'>
+        { items?.map((item, i) => (
+          <Card 
+            key={i}
+            data={item}
+          />
+        )) }
+      </div>
+      <ProductDetail />
+    </Layout>
   )
 }
 
