395b508bc11b7742eb728963a4bb52970e9f659c
afmurillo97 <afmurillo97@gmail.com>
2024-05-27 20:06:50 -0500

    add dinamic classes and icons

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,67 +1,134 @@
-import { NavLink } from "react-router-dom";
+import { 
+  AtSymbolIcon, 
+  ClipboardDocumentCheckIcon, 
+  WrenchScrewdriverIcon, 
+  UserCircleIcon,
+  ShoppingBagIcon
+} from '@heroicons/react/24/outline';
+import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+import { ShoppingCartContext } from '../../Context';
+
 
 const Navbar = () => {
+
+  const context = useContext(ShoppingCartContext);
+  const activeStyle = 'underline underline-offset-4 text-white';
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/'>
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light bg-red-400 shadow-md'>
+      <ul className='flex items-center gap-3'>
+        <li className='font-semibold text-lg'>
+          <NavLink to = '/'>
             Shopi
           </NavLink>
         </li>
         <li>
-          <NavLink to='/'>
+          <NavLink 
+            to = '/'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
             All
           </NavLink>
         </li>
         <li>
-          <NavLink to='/Clothes'>
+          <NavLink 
+            to = '/Clothes'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
             Clothes
           </NavLink>
         </li>
         <li>
-          <NavLink to='/Electronics'>
+          <NavLink 
+            to = '/Electronics'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
             Electronics
           </NavLink>
         </li>
         <li>
-          <NavLink to='/Furnitures'>
+          <NavLink 
+            to = '/Furnitures'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
             Furnitures
           </NavLink>
         </li>
         <li>
-          <NavLink to='/Toys'>
+          <NavLink 
+            to = '/Toys'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
             Toys
           </NavLink>
         </li>
         <li>
-          <NavLink to='/Others'>
+          <NavLink 
+            to = '/Others'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
             Others
           </NavLink>
         </li>
       </ul>
 
-      <ul>
-        <li>
-          afmurillo97@gmail.com
+      <ul className='flex items-center gap-3'>
+        <li className='text-black/60  flex items-center'>
+          <span>afmurillo97</span>
+          <AtSymbolIcon className='size-5'/>
+          <span>gmail.com</span>
         </li>
-        <li>
-          <NavLink to='/my-orders'>
-            My Orders
+        <li className='flex items-center'>
+          <NavLink 
+            to='/my-orders'
+            className={( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
+            <ClipboardDocumentCheckIcon className='size-4'/>
+            <span>My Orders</span>
+          </NavLink>
+        </li>
+        <li className='flex flex-col items-center'>
+          <NavLink 
+            to='/my-account'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
+            <WrenchScrewdriverIcon className='size-4'/>
+            <span>My Account</span>
           </NavLink>
         </li>
         <li>
-          <NavLink to='/my-account'>
-            My Account
+          <NavLink 
+            to='/sign-in'
+            className = {( {isActive} ) => 
+              isActive ? activeStyle : undefined
+            }
+          >
+            <UserCircleIcon className='size-4'/>
+            <span>Sign In</span>
           </NavLink>
         </li>
-        <li>
-          <NavLink to='/sign-in'>
-            Sign In
-          </NavLink>
-        </li>
-        <li>
-          CC 0
+        <li className='relative cursor-pointer'>
+          <span className='absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 bg-yellow-400 rounded-full w-5 h-5 flex items-center justify-center text-xs'>
+            {context.count}
+          </span>
+          <ShoppingBagIcon className='size-7'/>
         </li>
       </ul>
     </nav>
