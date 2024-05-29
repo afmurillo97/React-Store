824e0caba598a4226cffe7d09ae7f4cea8638a4a
afmurillo97 <afmurillo97@gmail.com>
2024-05-28 21:11:07 -0500

    update ProductDetail, add other section

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -7,22 +7,50 @@ const ProductDetail = () => {
 
   const context = useContext(ShoppingCartContext);
 
-  const openProductDetail = context.isProductDetailOpen ? 'flex' : 'hidden';
+  // Animations available: slideInRight, fadeInUp, scaleIn
+  const openProductDetailClasses = context.isProductDetailOpen ? 'animate-slideInRight' : 'animate-slideOutLeft';
 
   return (
-    <aside 
-      className={`${openProductDetail} product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white`}
-    >
-      <div className='flex justify-between items-center p-6'>
-        <h2 className='font-medium text-xl'>Detail</h2>
-        <div 
-          className='product-close-detail'
-          onClick={() => context.toogleProductDetail(true)}
-        >
-          <XMarkIcon className='size-6' />
+
+    context.isProductDetailVisible && (
+      <aside 
+        className={`${openProductDetailClasses} product-detail flex flex-col fixed right-0 top-0 mt-20 border-4 border-red-400 rounded-lg bg-white overflow-y-auto hover:overflow-y-scroll max-h-screen`}
+      >
+        <div className='flex justify-between items-center p-6'>
+          <h2 className='font-medium text-xl'>Detail</h2>
+          <div 
+            className='product-detail-close'
+            onClick={() => context.closeProductDetail()}
+          >
+            <XMarkIcon className='size-6' />
+          </div>
         </div>
-      </div>
-    </aside>
+
+        <figure className='px-6 justify-center items-center'>
+          <img 
+            className='w-full max-h-48 max-w-full object-contain rounded-lg' 
+            src={ context.productToShow.image } 
+            alt={ context.productToShow.title } 
+          />
+        </figure>
+
+        <div className='flex flex-col p-6 space-y-2'>
+          <span className='font-bold text-3xl text-green-500 px-2 rounded-md bg-white'>
+            ${context.productToShow.price}
+          </span>
+          <span className='font-semibold text-xl text-gray-800 px-2 rounded-md bg-white'>
+            {context.productToShow.title}
+          </span>
+          <div className='border-t-2 border-red-400 my-4'></div>
+          <textarea
+            className='font-light text-sm w-full h-32 resize-none p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-400 shadow-inner'
+            readOnly
+            value={context.productToShow.description}
+          />
+        </div>
+      </aside>
+    )
+    
   );
 
 };
