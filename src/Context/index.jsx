64f3c2121eb173c77af890ccff5fe1202f3ffef3
afmurillo97 <afmurillo97@gmail.com>
2024-05-28 21:11:34 -0500

    new functions added

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,18 +4,68 @@ import { createContext, useState } from 'react';
 const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+  // Product Detail: Manage data
+  const getFirstThreeWords = (text) => {
+    const words = text.split(' ');
+    const firstThreeWords = words.slice(0, 3);
 
+    return firstThreeWords.join(' ');
+  };
+
+  // Product Detail: Cart counter
   const [count, setCount] = useState(0);
-  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
-
-  const toogleProductDetail = () => (setIsProductDetailOpen(!isProductDetailOpen));
   
+  // Product Detail: Open/Close aside
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const [isProductDetailVisible, setIsProductDetailVisible] = useState(false);
+  const openProductDetail  = () => {
+    setIsProductDetailVisible(true);
+    setIsProductDetailOpen(true);
+  };
+  const closeProductDetail  = () => {
+    setIsProductDetailOpen(false);
+    setTimeout(() => {
+      setIsProductDetailVisible(false);
+    }, 500)
+  };
+
+  // Checkout Side Menu: Open/Close aside
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
+  const [isCheckoutSideMenuVisible, setIsCheckoutSideMenuVisible] = useState(false);
+  const openCheckoutSideMenu  = () => {
+    setIsCheckoutSideMenuVisible(true);
+    setIsCheckoutSideMenuOpen(true);
+  };
+  const closeCheckoutSideMenu  = () => {
+    setIsCheckoutSideMenuOpen(false);
+    setTimeout(() => {
+      setIsCheckoutSideMenuVisible(false);
+    }, 500)
+  };
+  
+  // Product Detail: Show Product
+  const [productToShow, setProductToShow] = useState({});
+
+  // Product Detail: Add products to cart
+  const [cartProducts, setCartProducts] = useState([]);
+
   return (
     <ShoppingCartContext.Provider value={{
+      getFirstThreeWords,
       count,
-      setCount,
       isProductDetailOpen,
-      toogleProductDetail
+      isProductDetailVisible,
+      isCheckoutSideMenuOpen,
+      isCheckoutSideMenuVisible,
+      productToShow,
+      cartProducts,
+      setCount,
+      setProductToShow,
+      setCartProducts,
+      openProductDetail,
+      closeProductDetail,
+      openCheckoutSideMenu,
+      closeCheckoutSideMenu,
     }}>
       { children }
     </ShoppingCartContext.Provider>
