b0664ea5c8fd6b50ed5093ce38e04dfd69cb8a42
afmurillo97 <afmurillo97@gmail.com>
2024-05-28 21:09:12 -0500

    update buttons and add functions

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,38 +1,66 @@
-import { PlusIcon } from '@heroicons/react/24/outline';
+import { CheckBadgeIcon } from '@heroicons/react/24/outline';
 import PropTypes from 'prop-types';
 import { useContext } from 'react';
 import { ShoppingCartContext } from '../../Context';
+import IconPlus from '../Icons/IconPlus';
 
 const Card = ({ data }) => {
 
   const context = useContext(ShoppingCartContext);
 
-  function getFirstThreeWords(text) {
-    const words = text.split(' ');
-    const firstThreeWords = words.slice(0, 3);
+  const showProduct = (productDetail) => {
+    context.closeCheckoutSideMenu();
+    context.openProductDetail();
+    context.setProductToShow(productDetail);
+  };
 
-    return firstThreeWords.join(' ');
-  }
+  const addProductToCart = (event, productData) => {
+    event.stopPropagation();
+    context.closeProductDetail();
+
+    const products = context.cartProducts;
+    const exists = products.some(item => item.id === productData.id);
+
+    if (!exists) {
+      context.setCartProducts([productData, ...products]);
+      context.setCount(context.count + 1);
+    }
+    
+    context.openCheckoutSideMenu();
+  };
+
+  const renderIcon = (id) => {
+    const productObj = context.cartProducts.find(product => product.id === id);
+
+    const icon = (productObj === undefined) ? 
+      <IconPlus title='Add Product'/> : <CheckBadgeIcon className='size-5'/>;
+
+    const style = (productObj === undefined) 
+      ? 'bg-red-400 bg-opacity-40 hover:bg-red-400 hover:text-white transition duration-300 ease-in-out' 
+      : 'bg-green-500 cursor-not-allowed';
+
+    return (
+      <div 
+          className={`${style} absolute top-0 right-0 flex justify-center items-center w-8 h-8 rounded-full m-2 p-1`}
+          onClick={(event) => addProductToCart(event, data)}
+        >
+          { icon }
+      </div>
+    );
+  };
 
   return (
     <div 
       className='bg-white cursor-pointer w-44 h-50 rounded-lg'
-      onClick={() => context.toogleProductDetail(false)}
+      onClick={() => showProduct(data)}
     >
       <figure className='relative md-2 w-full h-4/5'>
         <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{ data.category }</span>
         <img className='w-full h-full object-cover rounded-lg' src={ data.image } alt={ data.title } />
-        <div 
-          className='absolute top-0 right-0 flex justify-center items-center bg-red-400 bg-opacity-40 w-8 h-8 rounded-full m-2 p-1'
-          onClick={() => 
-            context.setCount(context.count + 1)
-          }
-        >
-          <PlusIcon className='size-5'/>
-        </div>
+        { renderIcon(data.id) }
       </figure>
       <p className='flex justify-between'>
-        <span className='text-xs font-light'>{ getFirstThreeWords(data.title) }</span>
+        <span className='text-xs font-light'>{ context.getFirstThreeWords(data.title) }</span>
         <span className='text-sm font-medium'>${ data.price }</span>
       </p>
     </div>
@@ -42,6 +70,7 @@ const Card = ({ data }) => {
 
 Card.propTypes = {
   data: PropTypes.shape({
+    id: PropTypes.number.isRequired,
     category: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
