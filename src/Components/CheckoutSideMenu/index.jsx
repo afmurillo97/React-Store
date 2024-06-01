c92930a960ef37e58904e2b149c08373f51e6f36
afmurillo97 <afmurillo97@gmail.com>
2024-05-31 20:26:53 -0500

    update checkout action and new button style

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,21 +1,39 @@
-import { XMarkIcon } from '@heroicons/react/20/solid';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { XMarkIcon, CreditCardIcon } from '@heroicons/react/20/solid';
 import { ShoppingCartContext } from '../../Context';
 import OrderCard from '../../Components/OrderCard';
 import { totalPrice } from '../../utils';
-import { useContext } from 'react';
 import './styles.css';
 
 const CheckoutSideMenu = () => {
 
   const context = useContext(ShoppingCartContext);
 
-  const openCheckoutSideMenu = context.isCheckoutSideMenuOpen ? 'animate-fadeInUp' : 'animate-fadeOutDown';
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id !== id);
+
+    context.setCartProducts(filteredProducts);
+  };
+
+  const handleCheckout = () => {
+    const orderToAdd = {
+      date: '01.02.2024',
+      products: context.cartProducts,
+      totalProducts: context.cartProducts.length,
+      totalPrice: totalPrice(context.cartProducts)
+    }
+
+    context.setOrders([...context.orders, orderToAdd]);
+    context.setCartProducts([]);
+    context.closeCheckoutSideMenu();
+  };
 
   return (
 
     context.isCheckoutSideMenuVisible && (
       <aside 
-        className={`${openCheckoutSideMenu} checkout-side-menu flex flex-col fixed right-0 top-0 mt-20 border-4 border-red-400 rounded-lg bg-white overflow-y-auto hover:overflow-y-scroll max-h-screen`}
+        className={`${context.isCheckoutSideMenuOpen ? 'animate-fadeInUp' : 'animate-fadeOutDown'} checkout-side-menu flex flex-col fixed right-0 top-0 mt-20 border-4 border-red-400 rounded-lg bg-white overflow-y-auto hover:overflow-y-scroll max-h-screen`}
       >
         <div className='flex justify-between items-center p-6'>
           <h2 className='font-medium text-xl'>My Order</h2>
@@ -32,6 +50,25 @@ const CheckoutSideMenu = () => {
             <span className='font-light'>Total: </span>
             <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
           </p>
+          {(context.cartProducts.length > 0) && (
+            <Link to='/my-orders/last'>
+              <button
+                className='group flex items-center justify-center w-11 h-11 bg-black rounded-full cursor-pointer relative overflow-hidden transition-all duration-200 shadow-lg hover:w-full hover:rounded-lg active:translate-x-1 active:translate-y-1'
+                onClick={() => handleCheckout()}
+              >
+                <div
+                  className='flex items-center justify-center w-full transition-all duration-300 group-hover:justify-start group-hover:px-3'
+                >
+                  <CreditCardIcon className='size-6' fill='white'/>
+                </div>
+                <div
+                  className='absolute right-5 transform translate-x-full opacity-0 text-white text-lg font-semibold transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100'
+                >
+                  Checkout
+                </div>
+              </button>
+            </Link>
+          )}
         </div>
 
         <div className='border-t-2 border-red-400 m-4'></div>
@@ -41,9 +78,11 @@ const CheckoutSideMenu = () => {
             context.cartProducts.map(product => (
               <OrderCard 
                 key={product.id}
+                id={product.id}
                 title={context.getFirstThreeWords(product.title)} 
                 imageUrl={product.image} 
                 price={product.price}
+                handleDelete={handleDelete}
               />
             ))
           }
