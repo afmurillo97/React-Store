c160e2943c447f6f44f176176920143a0f7943c5
afmurillo97 <afmurillo97@gmail.com>
2024-05-31 20:27:29 -0500

    create new barcode icon

---
+++ src/Components/Icons/IconBarcode.jsx
@@ -0,0 +1,55 @@
+import './Barcode.css'
+
+function IconBarcode() {
+  return (
+    <svg className='' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 575.2 93.16">
+      <defs>
+          
+      </defs>
+      <path className="st1" d="M0.85,93.16V0m4,93.16V0m6,93.16V0"/>
+      <line className="st2" x1="17.85" y1="93.16" x2="17.85"/>
+      <line className="st3" x1="25.85" y1="93.16" x2="25.85"/>
+      <line className="st1" x1="32.85" y1="93.16" x2="32.85"/>
+      <line className="st4" x1="40.85" y1="93.16" x2="40.85"/>
+      <line className="st1" x1="46.85" y1="93.16" x2="46.85"/>
+      <line className="st2" x1="51.85" y1="93.16" x2="51.85"/>
+      <path className="st1" d="M60.85,93.16V0m10,93.16V0m4,93.16V0m6,93.16V0m8,93.16V0m4,93.16V0m4,93.16V0m4,93.16V0m6,93.16V0"/>
+      <line className="st4" x1="116.85" y1="93.16" x2="116.85"/>
+      <line className="st1" x1="122.85" y1="93.16" x2="122.85"/>
+      <line className="st4" x1="130.85" y1="93.16" x2="130.85"/>
+      <line className="st1" x1="138.85" y1="93.16" x2="138.85"/>
+      <path className="st2" d="M143.85,93.16V0m8,93.16V0m6,93.16V0m6,93.16V0"/>
+      <path className="st1" d="M170.85,93.16V0m6,93.16V0m8,93.16V0m4,93.16V0"/>
+      <path className="st1" d="M193.85,93.16V0m4,93.16V0m6,93.16V0"/>
+      <line className="st2" x1="210.85" y1="93.16" x2="210.85"/>
+      <line className="st3" x1="218.85" y1="93.16" x2="218.85"/>
+      <line className="st1" x1="225.85" y1="93.16" x2="225.85"/>
+      <line className="st4" x1="233.85" y1="93.16" x2="233.85"/>
+      <line className="st1" x1="239.85" y1="93.16" x2="239.85"/>
+      <line className="st2" x1="244.85" y1="93.16" x2="244.85"/>
+      <path className="st1" d="M253.85,93.16V0m10,93.16V0m4,93.16V0m6,93.16V0m8,93.16V0m4,93.16V0m4,93.16V0m4,93.16V0m6,93.16V0"/>
+      <line className="st4" x1="309.85" y1="93.16" x2="309.85"/>
+      <line className="st1" x1="315.85" y1="93.16" x2="315.85"/>
+      <line className="st4" x1="323.85" y1="93.16" x2="323.85"/>
+      <line className="st1" x1="331.85" y1="93.16" x2="331.85"/>
+      <path className="st2" d="M336.85,93.16V0m8,93.16V0m6,93.16V0m6,93.16V0"/>
+      <path className="st1" d="M363.85,93.16V0m6,93.16V0m8,93.16V0m4,93.16V0"/>
+      <path className="st1" d="M386.35,93.16V0m4,93.16V0m6,93.16V0"/>
+      <line className="st2" x1="403.35" y1="93.16" x2="403.35"/>
+      <line className="st3" x1="411.35" y1="93.16" x2="411.35"/>
+      <line className="st1" x1="418.35" y1="93.16" x2="418.35"/>
+      <line className="st4" x1="426.35" y1="93.16" x2="426.35"/>
+      <line className="st1" x1="432.35" y1="93.16" x2="432.35"/>
+      <line className="st2" x1="437.35" y1="93.16" x2="437.35"/>
+      <path className="st1" d="M446.35,93.16V0m10,93.16V0m4,93.16V0m6,93.16V0m8,93.16V0m4,93.16V0m4,93.16V0m4,93.16V0m6,93.16V0"/>
+      <line className="st4" x1="502.35" y1="93.16" x2="502.35"/>
+      <line className="st1" x1="508.35" y1="93.16" x2="508.35"/>
+      <line className="st4" x1="516.35" y1="93.16" x2="516.35"/>
+      <line className="st1" x1="524.35" y1="93.16" x2="524.35"/>
+      <path className="st2" d="M529.35,93.16V0m8,93.16V0m6,93.16V0m6,93.16V0"/>
+      <path className="st1" d="M556.35,93.16V0m6,93.16V0m8,93.16V0m4,93.16V0"/>
+    </svg>
+  );
+}
+
+export { IconBarcode };
