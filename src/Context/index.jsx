6669a33004e4a5876faeddee10debaf7fc0be212
afmurillo97 <afmurillo97@gmail.com>
2024-06-04 13:19:25 -0500

    update and organize context

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,9 +1,26 @@
 import PropTypes from 'prop-types';
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect, useCallback } from 'react';
+import { backupItems } from './fakeProducts';
 
 const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+  // Estado y funciones para manejar el detalle del producto, el carrito y las órdenes.
+  const [count, setCount] = useState(0);
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const [isProductDetailVisible, setIsProductDetailVisible] = useState(false);
+  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
+  const [isCheckoutSideMenuVisible, setIsCheckoutSideMenuVisible] = useState(false);
+  const [productToShow, setProductToShow] = useState({});
+  const [cartProducts, setCartProducts] = useState([]);
+  const [orders, setOrders] = useState([]);
+  const [items, setItems] = useState(null);
+  const [filteredItems, setFilteredItems] = useState(null);
+  const [searchByTitle, setSearchByTitle] = useState('');
+  const [categoryRequested, setCategoryRequested] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+
   // Product Detail: Manage data
   const getFirstThreeWords = (text) => {
     const words = text.split(' ');
@@ -12,49 +29,72 @@ export const ShoppingCartProvider = ({ children }) => {
     return firstThreeWords.join(' ');
   };
 
-  // Product Detail: Cart counter
-  const [count, setCount] = useState(0);
-  
-  // Product Detail: Open/Close aside
-  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
-  const [isProductDetailVisible, setIsProductDetailVisible] = useState(false);
-  const openProductDetail  = () => {
-    setIsProductDetailVisible(true);
-    setIsProductDetailOpen(true);
-  };
-  const closeProductDetail  = () => {
-    setIsProductDetailOpen(false);
-    setTimeout(() => {
-      setIsProductDetailVisible(false);
-    }, 500)
+  const filteredItemsByTitle = useCallback((items, search) => {
+    return items?.filter(item => item.title.toLowerCase().includes(search.toLowerCase()));
+  }, []);
+
+  const fetchItemsByCategory = async (category) => {
+    const categoryUrl = (category.length) ? 'category/' + category : '';
+    const response = await fetch(`https://fakest111oreapi.com/products/${categoryUrl}`);
+    return response.json();
   };
 
-  // Checkout Side Menu: Open/Close aside
-  const [isCheckoutSideMenuOpen, setIsCheckoutSideMenuOpen] = useState(false);
-  const [isCheckoutSideMenuVisible, setIsCheckoutSideMenuVisible] = useState(false);
-  const openCheckoutSideMenu  = () => {
-    setIsCheckoutSideMenuVisible(true);
-    setIsCheckoutSideMenuOpen(true);
-  };
-  const closeCheckoutSideMenu  = () => {
-    setIsCheckoutSideMenuOpen(false);
-    setTimeout(() => {
-      setIsCheckoutSideMenuVisible(false);
-    }, 500)
-  };
-  
-  // Product Detail: Show Product
-  const [productToShow, setProductToShow] = useState({});
+  // Product Detail: Open/Close modal and manage transitions
+  const openProductDetail = () => { setIsProductDetailVisible(true) || setIsProductDetailOpen(true) };
+  const closeProductDetail = () => { setIsProductDetailOpen(false) || setTimeout(() => setIsProductDetailVisible(false), 500) }
 
-  // Product Detail: Add products to cart
-  const [cartProducts, setCartProducts] = useState([]);
+  const openCheckoutSideMenu = () => { setIsCheckoutSideMenuVisible(true) || setIsCheckoutSideMenuOpen(true) };
+  const closeCheckoutSideMenu = () => { setIsCheckoutSideMenuOpen(false) || setTimeout(() => setIsCheckoutSideMenuVisible(false), 500) }
 
-  //Shopping Cart: Order
-  const [orders, setOrders] = useState([]);
+  //API proyect: Get all Items
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('https://fakest111oreapi.com/products')
+      .then(response => response.json())
+      .then(data => {
+        setItems(data);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching API products:', error);
+        setItems(backupItems);
+        setIsLoading(false);
+      })
+  }, []);
+
+  //API proyect: Get search term
+  useEffect(() => {
+    if (searchByTitle) {
+      setFilteredItems(filteredItemsByTitle(items, searchByTitle));
+    }
+  }, [items, searchByTitle, filteredItemsByTitle]);
+
+  //API proyect: filter by one or more categories
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchItemsByMultipleCategories = async (categories) => {
+      const promises = categories.map(category => fetchItemsByCategory(category));
+      const results = await Promise.all(promises);
+      return results.flat();
+    };
+
+    if (categoryRequested.length > 0) {
+      fetchItemsByMultipleCategories(categoryRequested)
+        .then(itemsByCategory => {
+          setItems(itemsByCategory);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.error('Error fetching products:', error);
+          const filteredBackupItems = backupItems.filter( item => categoryRequested.includes(item.category) )
+          setItems(filteredBackupItems.length > 0 ? filteredBackupItems : backupItems);
+          setIsLoading(false);
+        });
+    }
+  }, [categoryRequested]);
 
   return (
     <ShoppingCartContext.Provider value={{
-      getFirstThreeWords,
       count,
       isProductDetailOpen,
       isProductDetailVisible,
@@ -62,6 +102,12 @@ export const ShoppingCartProvider = ({ children }) => {
       isCheckoutSideMenuVisible,
       productToShow,
       cartProducts,
+      orders,
+      items,
+      filteredItems,
+      searchByTitle,
+      categoryRequested,
+      getFirstThreeWords,
       setCount,
       setProductToShow,
       setCartProducts,
@@ -69,13 +115,16 @@ export const ShoppingCartProvider = ({ children }) => {
       closeProductDetail,
       openCheckoutSideMenu,
       closeCheckoutSideMenu,
-      orders,
-      setOrders
+      setOrders,
+      setItems,
+      setSearchByTitle,
+      setFilteredItems,
+      setCategoryRequested,
+      isLoading
     }}>
-      { children }
+      {children}
     </ShoppingCartContext.Provider>
   );
-
 };
 
 ShoppingCartProvider.propTypes = {
